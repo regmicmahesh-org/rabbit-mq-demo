4dcb956a3e4c599d3ac549380925605bbed3e26d
Har Har Mahadev! <0xmahadev@gmail.com>
2021-01-20 19:34:10 +0545

    [add] added fanout exchange type program

---
+++ fanout-exchange/consumer.py
@@ -0,0 +1,29 @@
+#importing pika
+import pika
+
+#saving credentials to access the rabbitmq server
+credentials = pika.PlainCredentials(username='guest', password='guest')
+
+
+#establishing connection to rabbitmq server
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='10.6.0.7', credentials=credentials))
+
+#establishing a channel to communicate with exchange
+channel = connection.channel()
+
+
+#callback function which will be called on every new message on the queue.
+def callback(ch, method, properties , body):
+    body_str = body.decode('utf-8')
+    print("Received:\n ", body, "\nEnd")
+    #print(method)
+    channel.basic_ack(method.delivery_tag, False)
+    # acking the message manually because why not
+    #channel.basic_nack(method.delivery_tag,False, True)
+
+#consuming / listening to a queu called my.queue1 and disabling auto_ack
+channel.basic_consume("my.queue1", callback, False)
+
+print("Waiting for messages!")
+#start consuming/
+channel.start_consuming()
---
+++ fanout-exchange/producer.py
@@ -0,0 +1,44 @@
+import pika
+
+
+#establishing connection with rabbitMQ server
+connection = pika.BlockingConnection(pika.ConnectionParameters('10.6.0.7'))
+
+#creating a new channel for exchanging messages.
+channel = connection.channel()
+
+#declaring a new exchange to produce traffic into
+channel.exchange_declare("ex.fanout", "fanout",durable=True, auto_delete=False )
+
+#declaring two queues to send messages.
+channel.queue_declare(queue='my.queue2', durable=True,exclusive=False, auto_delete=False)
+
+channel.queue_declare(queue='my.queue1', durable=True,exclusive=False, auto_delete=False)
+
+#binding queues to the exchange.
+channel.queue_bind("my.queue1", "ex.fanout", "")
+channel.queue_bind("my.queue2", "ex.fanout", "")
+
+#publishing message to the exchange
+channel.basic_publish("ex.fanout","",b"hello World!")
+channel.basic_publish("ex.fanout","",b"hello World two!")
+print("Connection is ready [ send end to quit ]")
+msg = ""
+while msg != "end":
+    msg = input("Enter your message: ")
+    msg_en = msg.encode('utf-8')
+    channel.basic_publish("ex.fanout", "", msg_en )
+
+#waiting to check on consumer before removing the queue and exchange.
+
+#cleaning all queues and exchanges
+channel.queue_delete("my.queue1");
+channel.queue_delete("my.queue2");
+
+channel.exchange_delete("ex.fanout");
+
+channel.close();
+connection.close();
+
+#checking if all works.
+print("Deleted all messages")
